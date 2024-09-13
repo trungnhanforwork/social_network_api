d22d561fe2fe5df3f6ed0e10da7ec81fd389cdb2
trungnhan <trungnhanforwork@gmail.com>
2024-09-13 09:20:46 +0700

    feat: changing password with sending request email to change

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,23 @@ class User < ApplicationRecord
   has_many :follower_relationships, foreign_key: :followed_id, class_name: "Follow", dependent: :destroy
   has_many :followers, through: :follower_relationships, source: :follower
 
+  def generate_password_reset_token!
+    self.reset_password_token = SecureRandom.urlsafe_base64
+    self.reset_password_sent_at = Time.now.utc
+    save!
+  end
+
+  def password_token_valid
+    reset_password_sent_at < 1.hours.ago
+  end
+
+  def reset_password!(new_password)
+    # update reset_password_token field to be null after reset password handle
+    self.reset_password_token = nil
+    self.password = new_password
+    save!
+  end
+
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_11_095556) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_13_014644) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -85,6 +85,8 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_11_095556) do
     t.text "bio"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
