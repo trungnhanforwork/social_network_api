152a3eda1cc93c2c267c25c5ab1a4c3246311cae
trungnhan <trungnhanforwork@gmail.com>
2024-09-16 14:40:29 +0700

    refactor: convention return for delete post mutation

--- app/graphql/mutations/delete_post_mutation.rb
+++ app/graphql/mutations/delete_post_mutation.rb
@@ -1,27 +1,32 @@
 module Mutations
   class DeletePostMutation < Mutations::BaseMutation
-    argument :id, GraphQL::Types::ID, required: true
+    argument :id, ID, required: true
 
-    field :errors, [String], null: false
+    field :post, Types::PostType, null: true
+    field :errors, [ String ], null: false
+
     def resolve(id:)
       user = context[:current_user]
-      return {post: nil, errors:["Unauthorized"]} if user.nil? || user.id != context[:current_user].id
-      post = Post.find(id: id)
+
+      if user.nil? || user.id != context[:current_user].id
+        return { post: nil, errors: ["Unauthorized"] }
+      end
+
+      post = Post.find_by(id: id)
+
       if post.nil?
         raise GraphQL::ExecutionError, "Post with id = #{id} could not be found"
       end
-      if post.destroy()
-        return  {
-          errors: [],
-        }
+
+      if post.destroy
+        { post: post, errors: [] }
       else
-        return GraphQL::ExecutionError, post.errors.full_messages
+        raise GraphQL::ExecutionError, post.errors.full_messages.join(', ')
       end
+    rescue GraphQL::ExecutionError => e
+      { post: nil, errors: [ e.message ] }
     rescue StandardError => e
-      return {
-        post: nil,
-        errors: ["An error occured while deleting the post: #{e.message}"]
-      }
+      { post: nil, errors: [ "An error occurred while deleting the post: #{e.message}" ] }
     end
   end
 end
